50870d94629c74d91560b8622563790765b745c0
zfj321 <zfj321@qq.com>
2015-08-12 10:04:44 +0800

    提交所有

--- DayStar/test/SrcTest.java
+++ DayStar/test/SrcTest.java
@@ -1,10 +1,32 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.aopalliance.aop.Advice;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 public class SrcTest {
 
 	public static void main(String[] args) {
-		Class c1 = DataSourceTransactionManager.class;
-		Class c2= Advice.class;
+		// Class c1 = DataSourceTransactionManager.class;
+		// Class c2 = Advice.class;
+		// System.out.println(String.class.getSimpleName());
+		// Class c3 = HashMap.class;
+		// HashMap map = new HashMap();
+		// List list = new ArrayList();
+
+		List<Integer> li = new ArrayList<Integer>();
+		li.add(1);
+		li.add(2);
+		li.add(3);
+		li.add(4);
+		for (int i = 0; i < li.size(); i++) {
+			System.out.println("====");
+			li.remove(i);
+			i--;
+		}
+		System.out.println(li.size());
 	}
+
 }
